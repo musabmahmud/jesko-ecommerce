95095f6f8146366ae80577fcf83c478a9d1cf977
musabmahmud <md.musabmahmud100@gmail.com>
2021-10-22 02:43:06 +0600

    frontend product uploaded

--- app/Http/Controllers/FrontEndController.php
+++ app/Http/Controllers/FrontEndController.php
@@ -4,15 +4,22 @@ namespace App\Http\Controllers;
 
 use App\Models\Product;
 use Illuminate\Http\Request;
+use Illuminate\Support\Str;
+use Illuminate\Support\Carbon;
 
 class FrontEndController extends Controller
 {
     public function home(){
         return view('frontend.index',[
-            'latests' => Product::latest()->limit(15)->get(),
-            'men' => Product::where('type_name','men')->limit(8)->get(),
-            'women' => Product::where('type_name','women')->limit(8)->get(),
-            'kids' => Product::where('type_name','kids')->limit(8)->get(),
+            'allProducts' => Product::latest()->limit(8)->get(),
+            'latests' => Product::where('type_name','default')->latest()->limit(8)->get(),
+            'men' => Product::where('type_name','men')->latest()->limit(8)->get(),
+            'women' => Product::where('type_name','women')->latest()->limit(8)->get(),
+            'kids' => Product::where('type_name','kids')->latest()->limit(8)->get(),
+
+            'newProducts' => Product::whereDate('created_at', Carbon::yesterday())->limit(4)->get(),
+            'topArrivals' => Product::whereDate('created_at', '>=',date('Y-m'))->latest()->limit(4)->get(),
+            'bestSells' => Product::latest()->limit(4)->get(),
         ]);
     }
 }
